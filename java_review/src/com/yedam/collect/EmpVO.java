5e2f5048a87bbf44deec88eeb89c72ff5358008b
gywns <gywns8339@naver.com>
2025-08-29 18:00:15 +0900

    first commit

---
+++ java_review/src/com/yedam/collect/EmpVO.java
@@ -0,0 +1,16 @@
+package com.yedam.collect;
+
+import lombok.Data;
+
+//@Getter
+//@Setter
+//@ToString
+//@AllArgsConstructor
+//@RequiredArgsConstructor
+@Data
+public class EmpVO {
+	//필드 선언
+	final private int empno;
+	final private String ename;
+	private int dept;
+}
---
+++ java_review/src/com/yedam/inter/Cat.java
@@ -0,0 +1,9 @@
+package com.yedam.inter;
+
+public class Cat implements Animal{
+	
+	@Override
+	public void sound() {
+		System.out.println("야옹@!");
+	}
+}
